854f1bd138f64a64f2d822bec344f16666c4b5bd
Oren Goldberg <oren@oren-laptop.(none)>
2010-02-28 18:17:51 -0600

    added notes attribute and edited constraints for band.groovy

--- grails-app/domain/net/turfclub/Band.groovy
+++ grails-app/domain/net/turfclub/Band.groovy
@@ -6,15 +6,21 @@ class Band {
     String homePage
     String phone
     String contact
+    String notes
+
+    String toString() {
+       this.bandName
+    }
 
     static hasMany = [bookings : Booking]
 
     static constraints = {
-        bandName(nullable:false)
-        homePage(url:true)
-        email(email:true)
-        phone()
-        contact()
+        bandName(nullable:false, blank:false)
+        homePage(url:true, nullable:true, blank:true)
+        email(email:true, blank:true, nullable:true)
+        phone(blank:true, nullable:true)
+        contact(blank:true, nullable:true)
+        notes(blank:true, nullable:true, maxSize:2000)
 
     }
 }
