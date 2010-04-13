6018ab38dc0f11444918a3b2e8297013ad49ced4
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-04-13 18:48:59 -0500

    Improved documentation for parseDate method.

--- grails-app/services/net/turfclub/MiscService.groovy
+++ grails-app/services/net/turfclub/MiscService.groovy
@@ -7,6 +7,11 @@ class MiscService {
     // app-wide standard format for date input
     def pmFormat = 'MM/dd/yyyy h:mm a'
 
+    // Expects three things:
+    //     date in MM/dd/yyyy format
+    //     time in h:mm format (Ex: 9:40)
+    //     ampm in either "AM" or "PM" 
+    // Returns a Groovy date made up of the three parameters above.
     def parseDate(date, hour_min, ampm) {
         def d = Date.parse(pmFormat, date + ' ' + hour_min + ' ' + ampm)
         return d
