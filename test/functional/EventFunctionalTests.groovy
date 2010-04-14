74b9bb33941d15411f4ff3ef9b81fcaaa789f233
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-04-14 00:52:17 -0500

    Removed update test

--- test/functional/EventFunctionalTests.groovy
+++ test/functional/EventFunctionalTests.groovy
@@ -26,20 +26,4 @@ class EventFunctionalTests extends functionaltestplugin.FunctionalTestCase {
         assertContentContains 'April 20, 1999'
     }
 
-    void testUpdateEvent() {
-        loginAdmin()
-        get('/event/list')
-        form {
-            eventDate_value = '04/20/1999'
-            // you have to use a valid selection.  Since
-            // selections are 4:00, 4:30, 5:00, 5:30, 6:00, 6:30 etc,
-            // let's pick 4:30 (closest to 4:20 huh-huh)
-            eventTime = '4:30'
-            click "Create"
-        }
-
-        assertStatus 200
-        assertContentContains '4:30'
-        assertContentContains 'April 20, 1999'
-    }
 }
