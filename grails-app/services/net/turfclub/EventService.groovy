786e263a0a203a027367ab89a11eb31b4b2f2e9a
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-04-13 20:59:35 -0500

    Stub for eventService

--- grails-app/services/net/turfclub/EventService.groovy
+++ grails-app/services/net/turfclub/EventService.groovy
@@ -4,7 +4,21 @@ class EventService {
 
     boolean transactional = true
 
-    def serviceMethod() {
+    def todaysEvents() {
+        def cal = Calendar.instance
+        def year = cal.get(Calendar.YEAR) - 1900
+        def month = cal.get(Calendar.MONTH)
+        def day = cal.get(Calendar.DAY_OF_MONTH)
+
+        // println "Year = " + Y
+        // println "Month" + M
+        // println "Day" + D
+        def today = new Date(year, month, day)
+        // println " the first thingy today is: " + today
+        // println " the next date is : " + (today + 10)
+        
+        def todaysEvents = Event.findAllByEventDateBetween(today, (today + 1))
+        return todaysEvents
 
     }
 }
