a6505fb4c8a4b57a2c17f54820aa68feb0c2708e
oren <oren@oren-laptop.(none)>
2010-06-04 09:11:37 -0500

    copied enrollio code to allow logged in users to have access to all actions

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -5,7 +5,18 @@ import net.turfclub.*
  */
 class SecurityFilters {
     def filters = {
-        
+        // Ensure that all controllers and actions require an authenticated user,
+        auth(controller: "*", action: "*") {
+            before = {
+                // except for the "help" controller and the "error" controller
+                if (controllerName == "help" || controllerName == "error") {
+                    return true
+                }
+                // This just means that the user must be authenticated. He does 
+                // not need any particular role or permission. 
+                accessControl { true } 
+            } 
+        } 
                 
         auth(controller: "*", action: "*") {
             before = {
@@ -21,5 +32,6 @@ class SecurityFilters {
                 accessControl()
             }
         }
+        
     }
 }
