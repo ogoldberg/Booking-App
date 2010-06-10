915bcc1d49c36d32ed5b714039f091eb0f4fb328
oren <oren@oren-laptop.(none)>
2010-06-09 19:44:21 -0500

    required that bandName must be unique

--- grails-app/domain/net/turfclub/Band.groovy
+++ grails-app/domain/net/turfclub/Band.groovy
@@ -14,10 +14,10 @@ class Band implements Commentable {
        this.bandName
     }    
     
-    static hasMany = [bookings : Booking]
+    static hasMany = [bookings:Booking]
 
     static constraints = {
-        bandName(nullable:false, blank:false)
+        bandName(nullable:false, blank:false, unique:true)
         homePage(url:true, nullable:true, blank:true)
         email(email:true, blank:true, nullable:true)
         phone(blank:true, nullable:true)
