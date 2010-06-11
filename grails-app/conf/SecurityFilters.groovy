07fec52cc8ac931927b2ae30457e45fa0de5b9bd
oren <oren@oren-laptop.(none)>
2010-06-11 14:55:25 -0500

    cleaned up security filter so any logged in user can access the app

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -8,30 +8,17 @@ class SecurityFilters {
         // Ensure that all controllers and actions require an authenticated user,
         auth(controller: "*", action: "*") {
             before = {
-                // except for the "help" controller and the "error" controller
-                // if (controllerName == "help" || controllerName == "error") {
-                    // return true
-                // }
-                // This just means that the user must be authenticated. He does 
-                // not need any particular role or permission. 
-                accessControl { true } 
-            } 
-        } 
-                
-        auth(controller: "*", action: "*") {
-            before = {
-                // Ignore direct views (e.g. the default main index page).
                 if (controllerName == "event"
                         && (actionName == "todaysEvent"
                             || actionName == "futureEvents"
                             || actionName == "featuredEvents")) {
                     return true
                 }
-
-                // Access control by convention.
-                accessControl()
-            }
-        }
+                accessControl { true } 
+            } 
+        } 
+                
+       
         
     }
 }
